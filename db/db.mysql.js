a97b2675b5dcf4fb3bf0b04a5628319eeeae998b
Stéphane Rident <109585662+Golushi@users.noreply.github.com>
2023-03-27 21:28:48 +0200

    test db en ligne

--- db/db.mysql.js
+++ db/db.mysql.js
@@ -9,10 +9,16 @@ console.log(mysql);
 // Param connexion bdd
 const mysqlconnection = mysql.createConnection({
   host: "localhost",
-  database: "quai_antique",
-  user: "root",
-  password: "",
+  database: "id20467199_quai_antique	",
+  user: "id20467199_admin",
+  password: "tmY3bAWpW86W_O>1",
 });
+// const mysqlconnection = mysql.createConnection({
+//   host: "localhost",
+//   database: "quai_antique",
+//   user: "root",
+//   password: "",
+// });
 
 // Connecion bdd
 mysqlconnection.connect((err) => {
