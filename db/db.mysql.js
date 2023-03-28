cbb1b425183b641bea25d8380122640b518dcc15
Stéphane Rident <109585662+Golushi@users.noreply.github.com>
2023-03-28 11:01:46 +0200

    essai DB railway

--- db/db.mysql.js
+++ db/db.mysql.js
@@ -13,12 +13,22 @@ console.log(mysql);
 //   user: "id20467199_admin",
 //   password: "tmY3bAWpW86W_O>1",
 // });
-const mysqlconnection = mysql.createConnection({
-  host: "localhost",
-  database: "quai_antique",
-  user: "root",
-  password: "",
-});
+
+const urlsDB = `mysql://root:jKTJaBTY5nE6O9hxU6N4@containers-us-west-113.railway.app:7027/railway`;
+const mysqlconnection = mysql.createConnection(urlsDB);
+// const mysqlconnection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_DATABASE,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+// });
+
+// const mysqlconnection = mysql.createConnection({
+//   host: "localhost",
+//   database: "quai_antique",
+//   user: "root",
+//   password: "",
+// });
 
 // Connecion bdd
 mysqlconnection.connect((err) => {
