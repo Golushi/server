19a7e29a2298a599a7c578ac1c1765862b1f5d6b
Stéphane Rident <109585662+Golushi@users.noreply.github.com>
2023-04-07 08:54:25 +0200

    modif route sql

--- db/db.mysql.js
+++ db/db.mysql.js
@@ -6,7 +6,7 @@ dotenv.config();
 const mysql = require("mysql2");
 
 // Param connexion bdd
-const urlDB = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.PORT}/${process.env.DB_DATABASE}`;
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
 
 // Connection à la base de données
 const connection = mysql.createConnection(urlDB);
